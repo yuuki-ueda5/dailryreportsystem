bad7272ec075eb5c4e5d6e3ec1af65ed84c50a0f
yuuki.ueda <yuuki.1673.b@gmail.com>
2021-07-03 20:56:48 +0900

    Add Report edit

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Employee;
+import models.Report;
 import utils.DBUtil;
 
 /**
@@ -33,15 +34,18 @@ public class ReportsEditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+        Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        request.setAttribute("employee", e);
-        request.setAttribute("_token", request.getSession().getId());
-        request.getSession().setAttribute("employee_id", e.getId());
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(r != null && login_employee.getId() == r.getEmployee().getId()) {
+            request.setAttribute("report", r);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("report_id", r.getId());
+        }
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/edit.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
         rd.forward(request, response);
     }
 
